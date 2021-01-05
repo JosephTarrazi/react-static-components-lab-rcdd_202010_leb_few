3379d58f8d540bd3c845f5e45b5c9c8d906d417e
Joseph Tarrazi <joseftaerrazi@gmail.com>
2021-01-05 15:13:44 +0000

    Automatically backed up by Learn

--- src/CatComponent.js
+++ src/CatComponent.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react';
 export default class CatComponent extends Component {
   render() {
     return (
-      
       <div className="bar" id="cat">
-      // eslint-disable-next-line
         <img src="/cat.gif" alt="" />
       </div>
     );
